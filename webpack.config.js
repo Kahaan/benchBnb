785cd8d1c214d23510778bc8d97d1e47ed2851aa
Kahaan <kahaan.r.patel@gmail.com>
2019-02-15 20:54:27 -0800

    implemented session and user controller

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,27 @@
-var path = require("path");
-var webpack = require("webpack");
-
-var plugins = []; // if using any plugins for both dev and production
-var devPlugins = []; // if using any plugins for development
-
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    "process.env": {
-      NODE_ENV: JSON.stringify("production")
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === "production" ? prodPlugins : devPlugins
-);
+const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/index.jsx",
+  entry: "./frontend/bench_bnb.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
-  plugins: plugins,
   module: {
-    loaders: [
+    rules: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["env", "react"]
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/env", "@babel/react"]
+          }
         }
       }
     ]
   },
-  devtool: "source-maps",
+  devtool: "source-map",
   resolve: {
     extensions: [".js", ".jsx", "*"]
   }
