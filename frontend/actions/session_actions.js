df55bac5379177bf0267c73ca643a16e4a37673f
Kahaan <kahaan.r.patel@gmail.com>
2019-02-16 14:34:32 -0800

    dispatch action creators

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -17,3 +17,18 @@ export const receiveErrors = errors => ({
   type: RECEIVE_SESSION_ERRORS,
   errors
 });
+
+export const login = user => dispatch =>
+  APIUtil.login(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    err => dispatch(receiveErrors(err.responseJSON))
+  );
+
+export const signup = user => dispatch =>
+  APIUtil.signup(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    err => dispatch(receiveErrors(err.responseJSON))
+  );
+
+export const logout = () => dispatch =>
+  APIUtil.logout().then(user => dispatch(logoutCurrentUser()));
