604bb3b9374fe7586fd67c071422447c7422cffc
Kahaan <kahaan.r.patel@gmail.com>
2019-02-16 13:07:55 -0800

    User able to create session and login

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,7 +10,7 @@ class Api::SessionsController < ApplicationController
       login(@user)
       redirect_to root_url
     else
-      render(json: ["Invalid username/password combination"], status: 401)
+      render json: ["Invalid username/password combination"], status: 401
     end
   end
 
@@ -20,7 +20,7 @@ class Api::SessionsController < ApplicationController
       logout
       render json: {}
     else
-      render(json: ["Nobody signed in"], status: 404)
+      render json: ["Nobody signed in"], status: 404
     end
   end
 
