e7c8eaf50e0c38d57e755714a0aeeb1c1d71c9b1
Kahaan <kahaan.r.patel@gmail.com>
2019-02-18 14:15:53 -0800

    testing session

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -5,11 +5,23 @@ import configureStore from "./store/store";
 import { login, signup } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
-  window.login = login;
-  window.signup = signup;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  // window.login = login;
+  // window.signup = signup;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
