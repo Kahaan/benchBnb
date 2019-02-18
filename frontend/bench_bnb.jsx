a904c80e2727024acff822ec34c23606d4bdd529
Kahaan <kahaan.r.patel@gmail.com>
2019-02-18 11:36:37 -0800

    testing auth

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import configureStore from "./store/store";
 import Root from "./components/root";
+import configureStore from "./store/store";
+import { login } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
