a7c8d5005c0c539bd64a479aea65ee1b333e11af
Kahaan <kahaan.r.patel@gmail.com>
2019-02-18 20:23:01 -0800

    created benches api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'benches/index'
-    get 'benches/create'
-  end
-  get 'benches/index'
-  get 'benches/create'
+
   root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
+    resources :benches, only: [:index, :create]
   end
 
 end
