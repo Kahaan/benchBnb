5ffc7d03ec45f964a97f2abd92336a8c5da6a067
Kahaan <kahaan.r.patel@gmail.com>
2019-02-19 11:13:20 -0800

    bench index container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,6 +6,7 @@ import GreetingContainer from "./greeting/greeting_container";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
+import BenchIndexContainer from "./bench_index_container";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
 
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      <Route exact path="/" component={BenchIndexContainer} />
     </div>
   );
 };
