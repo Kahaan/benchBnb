a374ba25703053b3a0ab5f541c1a7821a307760a
Kahaan <kahaan.r.patel@gmail.com>
2019-02-19 16:34:31 -0800

    fixing bench index

--- frontend/components/bench_index_item.jsx
+++ frontend/components/bench_index_item.jsx
@@ -3,21 +3,13 @@ import React from "react";
 class BenchIndexItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      bench: ""
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.benches !== this.props.benches)
-      this.setState({ benches: this.props.benches });
   }
 
   render() {
     return (
-      <div>
-        <h1>{this.state.bench.id}</h1>
-        <h1>{this.state.bench.description}</h1>
+      <div className="BenchIndexItem">
+        <h4>{this.props.bench.id}</h4>
+        <h4>{this.props.bench.description}</h4>
       </div>
     );
   }
