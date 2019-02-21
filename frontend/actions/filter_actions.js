a0994a2fafe53d5e2618031e0688d7a022fa2524
Kahaan <kahaan.r.patel@gmail.com>
2019-02-21 15:30:30 -0800

    -a

--- frontend/actions/filter_actions.js
+++ frontend/actions/filter_actions.js
@@ -1,5 +1,3 @@
-import { fetchBenches } from "./bench_actions";
-
 export const UPDATE_BOUNDS = "UPDATE_BOUNDS";
 
 export const updateBounds = bounds => ({
--- frontend/components/search/search_container.js
+++ frontend/components/search/search_container.js
@@ -2,13 +2,15 @@ import { connect } from "react-redux";
 import React from "react";
 import Search from "./search";
 import { fetchBenches } from "../../actions/bench_actions";
+import { updateBounds } from "../../actions/filter_actions";
 import { asArray } from "../../reducers/selectors";
 
 const mapStateToProps = state => ({
   benches: asArray(state.entities)
 });
 const mapDispatchToProps = dispatch => ({
-  fetchBenches: () => dispatch(fetchBenches())
+  fetchBenches: () => dispatch(fetchBenches()),
+  updateBounds: bounds => dispatch(updateBounds(bounds))
 });
 
 export default connect(
