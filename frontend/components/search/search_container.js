fa4c74f13c9050c83d74ccb6acdbe02350f8ce8c
Kahaan <kahaan.r.patel@gmail.com>
2019-02-21 14:18:31 -0800

    rendering markers for map

--- frontend/components/search/search_container.js
+++ frontend/components/search/search_container.js
@@ -2,9 +2,10 @@ import { connect } from "react-redux";
 import React from "react";
 import Search from "./search";
 import { fetchBenches } from "../../actions/bench_actions";
+import { asArray } from "../../reducers/selectors";
 
 const mapStateToProps = state => ({
-  benches: state.entities.benches
+  benches: asArray(state.entities)
 });
 const mapDispatchToProps = dispatch => ({
   fetchBenches: () => dispatch(fetchBenches())
