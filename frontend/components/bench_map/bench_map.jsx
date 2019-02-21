39da8c74063d406cd52816a9aec2aa7d2dc00812
Kahaan <kahaan.r.patel@gmail.com>
2019-02-20 16:20:59 -0800

    added map

--- frontend/components/bench_map/bench_map.jsx
+++ frontend/components/bench_map/bench_map.jsx
@@ -5,8 +5,21 @@ class BenchMap extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 },
+      zoom: 13
+    };
+
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
+
   render() {
-    return <div id="map-container" ref="map" />;
+    return (
+      <div id="map-container" ref="map">
+        map
+      </div>
+    );
   }
 }
 
