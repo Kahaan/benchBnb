f7ba641eccdae2b16526298dd6533a9feaed6b33
Kahaan <kahaan.r.patel@gmail.com>
2019-02-22 12:11:29 -0800

    created bench form component

---
+++ frontend/components/bench_form/bench_form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class BenchForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default BenchForm;
