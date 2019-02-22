dc03f0b38a303f085609bc00cd700aad4a946682
Kahaan <kahaan.r.patel@gmail.com>
2019-02-22 12:10:19 -0800

    added seating to benches table

---
+++ frontend/components/bench_form/bench_form_container.js
@@ -0,0 +1,10 @@
+import { connect } from "react-redux";
+import BenchForm from "./bench_form";
+
+const mapStateToProps = () => ({});
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BenchForm);
