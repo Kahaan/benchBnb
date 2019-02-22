37ccff43fd1c0fc9a33808c4c5903d5ece375a2b
Kahaan <kahaan.r.patel@gmail.com>
2019-02-22 12:34:51 -0800

    route for new bench form

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -7,6 +7,7 @@ import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
 import SearchContainer from "./search/search_container";
+import BenchFormContainer from "./bench_form/bench_form_container";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
       <Route exact path="/" component={SearchContainer} />
+      <Route exact path="/benches/new" component={BenchFormContainer} />
     </div>
   );
 };
