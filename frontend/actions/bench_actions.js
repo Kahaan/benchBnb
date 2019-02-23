e9e08af29506c63e9b69d7f96da5aae7b6a94228
Kahaan <kahaan.r.patel@gmail.com>
2019-02-23 15:57:02 -0800

    update bench action creater

--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from "../util/bench_util";
 
 export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
 export const RECEIVE_BENCH = "RECEIVE_BENCH";
+export const UPDATE_FILTER = "UPDATE_FILTER";
 
 export const receiveBenches = benches => ({
   type: RECEIVE_BENCHES,
@@ -11,6 +12,12 @@ export const receiveBenches = benches => ({
 export const receiveBench = bench => ({
   type: RECEIVE_BENCH,
   bench
+});
+
+export const updateFilter = (filter, value) => ({
+  type: UPDATE_FILTER,
+  filter,
+  value
 });
 
 export const fetchBenches = () => dispatch =>
