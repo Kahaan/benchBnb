78599512cd9534c279bed3c054cff6e3ac6646e0
Kahaan <kahaan.r.patel@gmail.com>
2019-02-23 16:03:39 -0800

    -a

--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -2,7 +2,6 @@ import * as APIUtil from "../util/bench_util";
 
 export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
 export const RECEIVE_BENCH = "RECEIVE_BENCH";
-export const UPDATE_FILTER = "UPDATE_FILTER";
 
 export const receiveBenches = benches => ({
   type: RECEIVE_BENCHES,
@@ -12,12 +11,6 @@ export const receiveBenches = benches => ({
 export const receiveBench = bench => ({
   type: RECEIVE_BENCH,
   bench
-});
-
-export const updateFilter = (filter, value) => ({
-  type: UPDATE_FILTER,
-  filter,
-  value
 });
 
 export const fetchBenches = () => dispatch =>
--- frontend/actions/filter_actions.js
+++ frontend/actions/filter_actions.js
@@ -1,15 +1,20 @@
 import { fetchBenches } from "./bench_actions";
 
 export const UPDATE_BOUNDS = "UPDATE_BOUNDS";
+export const UPDATE_FILTER = "UPDATE_FILTER";
 
 export const updateBounds = bounds => ({
   type: UPDATE_BOUNDS,
   bounds
 });
 
-export function updateFilter(filter, value) {
-  return (dispatch, getState) => {
-    dispatch(changeFilter(filter, value));
-    return fetchBenches(getState().filters)(dispatch);
-  };
-}
+export const changeFilter = (filter, value) => ({
+  type: UPDATE_FILTER,
+  filter,
+  value
+});
+
+export const updateFilter = (filter, value) => (dispatch, getState) => {
+  dispatch(changeFilter(filter, value));
+  return fetchBenches(getState().ui.filters)(dispatch);
+};
