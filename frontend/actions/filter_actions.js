aaa6b6e03a437162bd44dab2d646dd6e2de68989
Kahaan <kahaan.r.patel@gmail.com>
2019-02-23 16:24:52 -0800

    updated filter actions

--- frontend/actions/filter_actions.js
+++ frontend/actions/filter_actions.js
@@ -1,12 +1,6 @@
 import { fetchBenches } from "./bench_actions";
 
-export const UPDATE_BOUNDS = "UPDATE_BOUNDS";
 export const UPDATE_FILTER = "UPDATE_FILTER";
-
-export const updateBounds = bounds => ({
-  type: UPDATE_BOUNDS,
-  bounds
-});
 
 export const changeFilter = (filter, value) => ({
   type: UPDATE_FILTER,
