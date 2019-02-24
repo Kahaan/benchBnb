397466566a06512e8c9489c9cc7b2b869a04a68e
Kahaan <kahaan.r.patel@gmail.com>
2019-02-23 16:08:16 -0800

    updated filters reducer

--- frontend/reducers/filter_reducer.js
+++ frontend/reducers/filter_reducer.js
@@ -1,11 +1,20 @@
 import merge from "lodash/merge";
-import { UPDATE_BOUNDS } from "../actions/filter_actions";
+import { UPDATE_FILTER } from "../actions/filter_actions";
 
-const filterReducer = (state = {}, action) => {
+const defaultFilters = Object.freeze({
+  bounds: {},
+  minSeating: 1,
+  maxSeating: 10
+});
+
+const filterReducer = (state = defaultFilters, action) => {
   Object.freeze(state);
   switch (action.type) {
-    case UPDATE_BOUNDS:
-      return merge({}, state, action.bounds);
+    case UPDATE_FILTER:
+      const newFilter = {
+        [action.filter]: action.value
+      };
+      return merge({}, state, newFilter);
     default:
       return state;
   }
