4fb4f1352d1bf8656e09431b8343d2dd1d58fe6f
Kahaan <kahaan.r.patel@gmail.com>
2019-02-24 10:48:54 -0800

    -a

---
+++ frontend/components/bench_show/bench_show.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class BenchShow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default BenchShow;
